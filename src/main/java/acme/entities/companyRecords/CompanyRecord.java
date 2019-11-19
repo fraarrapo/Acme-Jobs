14b830f51777d6c5a412a70c5f310d5602c5e0e9
Alejandro Blanco <Alejandro Blanco@10.100.243.213>
2019-11-19 13:45:22 +0100

    TASK 004:DONE!!

--- src/main/java/acme/entities/companyRecords/CompanyRecord.java
+++ src/main/java/acme/entities/companyRecords/CompanyRecord.java
@@ -2,6 +2,8 @@
 package acme.entities.companyRecords;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -17,6 +19,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "stars")
+})
 public class CompanyRecord extends DomainEntity {
 
 	//Serialisation identifier ---------------------------------------------
